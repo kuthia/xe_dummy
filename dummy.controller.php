e576d33df64f4cf3afe28e9630c7510d06b5e233
Professor X <x@w-coms.com>
2013-10-25 19:56:11 +0900

    fixed mistyped

--- dummy.controller.php
+++ dummy.controller.php
@@ -1,12 +1,12 @@
 <?php
 
 	/**
-	* @class 	dummyContoroller
+	* @class 	dummyController
 	* @author 	changeme (changeme@example.com)
 	* @brief 	dummy module controller
 	**/
 
-	class dummyContoroller extends dummy {
+	class dummyController extends dummy {
 	}
 
 ?>
